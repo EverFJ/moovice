734a065eefe85e28da4a31cf5a696372b0f35344
Francois Janson <jansonfrancois@gmail.com>
2021-09-10 12:45:33 +0200

    adding Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import WeeklyBattle from "./components/WeeklyBattle";
 import Popular from "./components/Popular";
 import PopularBattle from "./components/PopularBattle";
 import Error404 from "./components/Error404";
+import Navbar from "./components/Navbar";
 
 export default class App extends React.Component {
   render() {
@@ -14,52 +15,7 @@ export default class App extends React.Component {
     return (
       <>
         <BrowserRouter>
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="#">
-              Moovice
-            </a>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-toggle="collapse"
-              data-target="#navbarNav"
-              aria-controls="navbarNav"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-              <ul className="navbar-nav">
-                <li className="nav-item active">
-                  <Link className="nav-link" to="/">
-                    Home
-                    {/* <span className="sr-only">(current)</span> */}
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/weekly">
-                    Weekly
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/weekly-battle">
-                    Weekly Battle
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/popular">
-                    Popular
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/popular-battle">
-                    Popular Battle
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </nav>
+          <Navbar />
 
           <Switch>
             <Route exact path="/" component={Home} />
